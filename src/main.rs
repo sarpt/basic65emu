e3c18b5c077fbcf29058d6cfc128822cff84a061
sarpt <mike.m.info@gmail.com>
2025-02-26 19:33:16 +0100

    Do not inline cout and rdkey handler in monitoring vectors, instead jump to handlers stored in some free space

--- src/main.rs
+++ src/main.rs
@@ -15,46 +15,67 @@ mod memory;
 
 const KB9_BASIC_ROM_START_ADDR: Word = 0x2000;
 const KB9_COLD_START_ADDR: Word = 0x4065;
+const KB9_ROM_BIN_PATH: &str = "resources/kb9.bin";
 const KB9_MONCOUT_ADDR: Word = 0x1EA0;
 const KB9_MONRDKEY_ADDR: Word = 0x1E5A;
-const KB9_ROM_BIN_PATH: &str = "resources/kb9.bin";
+
+const KB9_MON_HANDLER_HI: Byte = 0xFF;
+const KB9_MONCOUT_HANDLER_LO: Byte = 0x00;
+const KB9_MONCOUT_STORE_LO: Byte = 0x04;
+const KB9_MONRDKEY_HANDLER_LO: Byte = 0x05;
+const KB9_MONRDKEY_STORE_LO: Byte = 0x0B;
 
 fn main() {
     let path: PathBuf = PathBuf::from(KB9_ROM_BIN_PATH);
     let mut mem = Generic64kMem::map_file(KB9_BASIC_ROM_START_ADDR, path).unwrap();
     mem.set_reset_vector(KB9_COLD_START_ADDR);
 
-    let moncout_handler = [
-        0x8D as Byte, // sta $FF00
-        0x00,
-        0xFF,
-        0xA9, // lda 1
-        0x01,
-        0x8D, // sta $FF01
-        0x01,
-        0xFF,
+    let moncout_jmp = [
+        0x20 as Byte, // jsr to handler location
+        KB9_MONCOUT_HANDLER_LO,
+        KB9_MON_HANDLER_HI,
         0x60, // rts
     ];
-    mem.insert(KB9_MONCOUT_ADDR, &moncout_handler);
+    mem.insert(KB9_MONCOUT_ADDR, &moncout_jmp);
+    let moncout_handler = [
+        0x8D as Byte, // sta to cout store location
+        KB9_MONCOUT_STORE_LO,
+        KB9_MON_HANDLER_HI,
+        0x60, // rts
+    ];
+    mem.insert(
+        Word::from_le_bytes([KB9_MONCOUT_HANDLER_LO, KB9_MON_HANDLER_HI]),
+        &moncout_handler,
+    );
 
+    let monrdkey_jump = [
+        0x20 as Byte, // jsr to handler location
+        KB9_MONRDKEY_HANDLER_LO,
+        KB9_MON_HANDLER_HI,
+        0x60, // rts
+    ];
+    mem.insert(KB9_MONRDKEY_ADDR, &monrdkey_jump);
     let monrdkey_handler = [
         0xA9 as Byte, // lda 13
         0x0D,
-        0x8D, // sta $FF02
-        0x02,
-        0xFF,
+        0x8D, // sta to rdkey store location
+        KB9_MONRDKEY_STORE_LO,
+        KB9_MON_HANDLER_HI,
         0x60, // rts
     ];
-    mem.insert(KB9_MONRDKEY_ADDR, &monrdkey_handler);
+    mem.insert(
+        Word::from_le_bytes([KB9_MONRDKEY_HANDLER_LO, KB9_MON_HANDLER_HI]),
+        &monrdkey_handler,
+    );
 
     let memory = RefCell::from(mem);
     let mut cpu = CPU::new_nmos(&memory);
     cpu.reset();
 
-    while !ready_to_read_key(memory.borrow().deref()) {
-        cpu.execute_next_instruction();
+    while !ready_to_read_key(memory.borrow().deref()) || !cpu.sync() {
+        cpu.tick();
 
-        if ready_to_output_character(memory.borrow().deref()) {
+        if cpu.sync() && ready_to_output_character(memory.borrow().deref()) {
             let out_character = consume_character(memory.borrow_mut().deref_mut());
             print!("{}", out_character);
         }
@@ -65,21 +86,22 @@ fn ready_to_read_key<T>(memory: &T) -> bool
 where
     T: Memory,
 {
-    return memory[0xFF02] != 0;
+    return memory[Word::from_le_bytes([KB9_MONRDKEY_STORE_LO, KB9_MON_HANDLER_HI])] != 0;
 }
 
 fn ready_to_output_character<T>(memory: &T) -> bool
 where
     T: Memory,
 {
-    return memory[0xFF01] == 1;
+    return memory[Word::from_le_bytes([KB9_MONCOUT_STORE_LO, KB9_MON_HANDLER_HI])] != 0;
 }
 
 fn consume_character<T>(memory: &mut T) -> char
 where
     T: Memory,
 {
-    let out_character = memory[0xFF00] as char;
-    memory[0xFF01] = 0;
+    let out_character =
+        memory[Word::from_le_bytes([KB9_MONCOUT_STORE_LO, KB9_MON_HANDLER_HI])] as char;
+    memory[Word::from_le_bytes([KB9_MONCOUT_STORE_LO, KB9_MON_HANDLER_HI])] = 0;
     return out_character;
 }
