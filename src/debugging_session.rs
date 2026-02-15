dd0cd5a863cd41326ad443cf8e8a14512f43ec20
sarpt <mike.m.info@gmail.com>
2026-02-15 22:40:23 +0100

    initiate debug regardless of debug log provided

--- src/debugging_session.rs
+++ src/debugging_session.rs
@@ -5,17 +5,31 @@ use std::{
   time::{self, UNIX_EPOCH},
 };
 
-use cpu6502::cpu::{CPU, debugger::Debugger};
+use cpu6502::cpu::{
+  CPU,
+  debugger::{Debugger, ProbeResult},
+};
+
+use crate::Addresses;
 
 pub struct DebuggingSession {
+  addresses: Addresses,
   debugger: Debugger,
-  debug_writer: BufWriter<File>,
+  debug_writer: Option<BufWriter<File>>,
 }
 
 const DEFAULT_DEBUG_BUFF_CAP_MB: usize = 2 * 1024 * 1024;
 
 impl DebuggingSession {
-  pub fn new(debugger: Debugger) -> Result<Self, String> {
+  pub fn new(debugger: Debugger, addresses: Addresses) -> Self {
+    DebuggingSession {
+      addresses,
+      debugger,
+      debug_writer: None,
+    }
+  }
+
+  pub fn initiate_log(&mut self) -> Result<(), String> {
     let dir_path = temp_dir().join(".basic65emu");
     create_dir_all(&dir_path)
       .map_err(|e| format!("could not create debug dir \"{}\": {e}", dir_path.display()))?;
@@ -34,29 +48,32 @@ impl DebuggingSession {
       .write(true)
       .open(path)
       .map_err(|e| format!("could not open debug file to write {e}"))?;
-    let debug_writer = BufWriter::with_capacity(DEFAULT_DEBUG_BUFF_CAP_MB, file_writer);
-    Ok(DebuggingSession {
-      debugger,
-      debug_writer,
-    })
+    self.debug_writer = Some(BufWriter::with_capacity(
+      DEFAULT_DEBUG_BUFF_CAP_MB,
+      file_writer,
+    ));
+
+    Ok(())
   }
 
-  pub fn probe(&mut self, cpu: &CPU) {
-    self.debugger.probe(cpu);
-    if let Some(inst) = self.debugger.get_last_instruction() {
-      _ = writeln!(&mut self.debug_writer, "{inst}");
+  pub fn probe(&mut self, cpu: &CPU) -> Vec<ProbeResult> {
+    let results = self.debugger.probe(cpu);
+    if let Some(inst) = self.debugger.get_last_instruction()
+      && let Some(debug_writer) = &mut self.debug_writer
+    {
+      _ = writeln!(debug_writer, "{inst}");
     }
+
+    results
   }
 
   pub fn close(&mut self) -> Result<(), String> {
-    self.debug_writer.flush().map_err(|e| format!("could not save debug buffer: {e}"))
-  }
-}
+    if let Some(debug_writer) = &mut self.debug_writer {
+      debug_writer
+        .flush()
+        .map_err(|e| format!("could not save debug buffer: {e}"))?;
+    }
 
-impl TryFrom<Debugger> for DebuggingSession {
-  type Error = String;
-
-  fn try_from(debugger: Debugger) -> Result<Self, Self::Error> {
-    DebuggingSession::new(debugger)
+    Ok(())
   }
 }
